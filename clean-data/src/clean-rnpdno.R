0a6b50fb7b661b772c5299020557f89729076b66
oeltonsu <oscar.elton@datacivica.org>
2020-10-14 12:03:56 -0500

    Edit/review clean-data task
    Former-commit-id: d2694025a3d0d78f527a6292fc87bb52500a0a96

--- clean-data/src/clean-rnpdno.R
+++ clean-data/src/clean-rnpdno.R
@@ -7,7 +7,7 @@
 
 
 if(!require(pacman))install.packages("pacman")
-pacman::p_load(tidyverse, janitor, lubridate, stringr, here)
+pacman::p_load(tidyverse, lubridate, here)
 
 # === File paths === #
 files <- list(pob = here("import/output/poblacion.rds"),
@@ -18,15 +18,11 @@ files <- list(pob = here("import/output/poblacion.rds"),
               out_years = here("clean-data/output/rnpdno-cenapi-years.rds"),
               out_rnpdno_nac = here("clean-data/output/rnpdno-estados.rds"),
               out_rnpdno_mor = here("clean-data/output/rnpdno-morelos.rds")
-)
-
+              )
 
 keep_years <- 2000:2020
 
-
 # === CENAPI y RNPDNO Morelos por año === #
-rnpdno_y <- readRDS(files$inp_rnpdno_y) 
-
 cenapi_rnpdno <- readRDS(files$inp_cenapi) %>%
   mutate(fecha_evento = word(fecha_evento, 1),
          fuente = "CENAPI",
@@ -45,24 +41,30 @@ cenapi_rnpdno <- readRDS(files$inp_cenapi) %>%
                              estatus == "VIVO" ~ "Localizado con vida",
                              estatus == "MUERTO" ~ "Localizado sin vida")) %>% 
   filter(year %in% keep_years & cve_ent == "17") %>% 
-  select(-fecha_evento, -nom_ent, -edad, -mes, -dia, -edad) %>% 
-  mutate(tot = 1) %>% 
   group_by(year, sexo, estatus, fuente) %>% 
-  summarise(tot = sum(tot, na.rm = T)) %>% 
-  rbind(rnpdno_y %>% 
-          mutate(year = as.integer(year),
-                 year = case_when(is.na(year) ~ as.integer(9999),
-                                  T ~ year)) %>% 
-          rename(Hombres = hombre, 
-                 Mujeres = mujer,
-                 Indeterminado = indeterminado) %>% 
-          pivot_longer(cols = -c(year, cve_ent, estatus),
-                       names_to = "sexo",
-                       values_to = "tot") %>%
-          filter(year %in% keep_years) %>% 
-          mutate(fuente = "RNPDNO") %>% 
-          select(-cve_ent))
+  summarise(tot = n()) %>% 
+  ungroup() %>% 
+  bind_rows(readRDS(files$inp_rnpdno_y) %>% 
+              mutate(year = as.integer(year),
+                     year = case_when(is.na(year) ~ as.integer(9999),
+                                      T ~ year)) %>% 
+              rename(Hombres = hombre, 
+                     Mujeres = mujer,
+                     Indeterminado = indeterminado) %>% 
+              pivot_longer(cols = -c(year, cve_ent, estatus),
+                           names_to = "sexo",
+                           values_to = "tot") %>%
+              filter(year %in% keep_years) %>% 
+              mutate(fuente = "RNPDNO") %>% 
+              select(-cve_ent))
 
+all <- cenapi_rnpdno %>% expand(crossing(year, sexo, estatus, fuente))
+
+cenapi_rnpdno <- right_join(cenapi_rnpdno, all) %>% 
+  replace_na(list(tot = 0)) %>% 
+  arrange(year, sexo, estatus, fuente)
+
+rm(all)
 saveRDS(cenapi_rnpdno, files$out_years)
 
 # === RNPDNO Nacional === #
@@ -78,29 +80,25 @@ rnpdno_nac <- readRDS(files$inp_rnpdno_nac) %>%
 
 saveRDS(rnpdno_nac, files$out_rnpdno_nac)
 
-
 # === RNPDNO Morelos por Municipio === #
-pob <- readRDS(files$pob) %>% 
-  filter(cve_ent == "17" & year == 2020) %>% 
-  group_by(municipio, sexo) %>% 
-  summarise(pob = sum(pob))
-
 rnpdno_mor <- readRDS(files$inp_rnpdno_mor) %>% 
   mutate(municipio = str_to_title(municipio),
          municipio = case_when(municipio %in% "Sin Municipio De Referencia" ~ "Se Desconoce",
                                T ~ municipio)) %>% 
  rename(Hombres = hombre, 
-         Mujeres = mujer,
-         Indeterminado = indeterminado) %>% 
+        Mujeres = mujer,
+        Indeterminado = indeterminado) %>% 
   pivot_longer(cols = -c(municipio, cve_ent, estatus),
                names_to = "sexo",
                values_to = "tot") %>%
   select(-cve_ent) %>% 
   group_by(municipio, estatus, sexo) %>% 
   summarise(tot = sum(tot)) %>% 
-  left_join(pob, by = c("sexo", "municipio"))
+  left_join(readRDS(files$pob) %>% 
+              filter(cve_ent == "17" & year == 2020) %>% 
+              group_by(municipio, sexo) %>% 
+              summarise(pob = sum(pob)))
 
 saveRDS(rnpdno_mor, files$out_rnpdno_mor)
-
 
 # done.
--- clean-data/src/clean-sesnsp.R
+++ clean-data/src/clean-sesnsp.R
@@ -7,37 +7,33 @@
 
 
 if(!require(pacman))install.packages("pacman")
-pacman::p_load(tidyverse, janitor, lubridate, stringr, here)
+pacman::p_load(tidyverse, lubridate, here)
 
 # === File paths === #
 files <- list(inp_sesnsp = here("import/output/sesnsp.rds"), 
               pob = here("import/output/poblacion.rds"),
               out_sesnsp = here("clean-data/output/sesnsp.rds")
-)
-
-# ==== Population ==== #
-pob <- readRDS(files$pob) %>% 
-  filter(cve_ent == "17") %>% 
-  group_by(year, cve_mun) %>% 
-  summarise(pob = sum(pob))
-
+              )
 
 # === SESNSP === #
 print("working in SESNSP")
 sesnsp <- readRDS(files$inp_sesnsp) %>% 
   filter(subtipo_de_delito %in% c("Feminicidio", "Homicidio doloso", "Tráfico de menores",
                                   "Otros delitos que atentan contra la libertad personal",
-                                  "Trata de personas")) %>% 
-  filter(cve_ent == "17") %>% 
+                                  "Trata de personas") & cve_ent == "17") %>% 
   rowwise() %>%
   mutate(tot = sum(across(enero:diciembre), na.rm = T)) %>% 
   ungroup() %>% 
   group_by(year, cve_mun, municipio, subtipo_de_delito) %>% 
   summarise(tot = sum(tot)) %>% 
   ungroup() %>% 
-  left_join(pob, by = c("cve_mun", "year"))
+  rename(inegi = cve_mun) %>% 
+  mutate(inegi = as.character(inegi)) %>% 
+  left_join(readRDS(files$pob) %>% 
+              filter(cve_ent == "17") %>% 
+              group_by(inegi, year) %>% 
+              summarise(pob = sum(pob))) # Se pega pob total porque se suma homicidio dolosos + feminicidio
   
 saveRDS(sesnsp, files$out_sesnsp)
 
-
-
+# done.
